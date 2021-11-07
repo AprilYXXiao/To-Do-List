2d2428de6e4876a2ced68b0bccdf33a0daa4ec09
AprilYXXiao <44380395+AprilYXXiao@users.noreply.github.com>
2021-11-07 00:20:26 -0700

    Update to_do_list.js

--- to_do_list.js
+++ to_do_list.js
@@ -1,98 +1,62 @@
-// // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HttpRequest API
-class ListItem {
-    constructor(id, title, isTask, isOverdue, counter) {
-      this.id = id;
-      this.title = title;
-      this.isTask = isTask;
-      this.isOverdue = isOverdue;
-      this.counter = counter;
+const View = (() => {
+    const domElements = {
+        keys: document.querySelector('#keys'),
+        values: document.querySelector('#values')
     }
-  }
-  
-  const itemArr = [
-    new ListItem(1, "Campaign Tasks", true, true, 1),
-    new ListItem(2, "Learning", false, false, 14),
-    new ListItem(3, "Stories", false, false, 2),
-    new ListItem(4, "Sc Task", true, true, 1),
-    new ListItem(5, "Portal Surveys", false, false, 2),
-    new ListItem(6, "Sport", false, false, 4),
-    new ListItem(7, "Sport", true, false, 7),
-  ];
+    const render = (element, tmp) => {
+        element.innerHTML = tmp;
+    }
+    return {
+        domElements,
+        render
+    }
+})();
 
-// create all task list
-function createAllListHTMLTemp(arr) {
-    let htmlTmp = "";
-    arr.forEach((ele) => {
-      htmlTmp += `<div class="row">
-          <div class="left">
-            <input type="checkbox" id="list1" name="list1" />
-            <label for="list1"> ${ele.title}</label>
-          </div>
-          <div class="right">
-            <button class="overdue-indicator"><span> ${ele.counter} Overdue</span></button>
-            <button class="duesoon-indicator"><span> ${ele.counter} Due Soon</span></button>
-          </div>
-          </div>`;
-    });
-    return htmlTmp;
-  }
+const Model = (() => {
+    const state = {
+        a: "apple",
+        b: "banana",
+        c: "city",
+        d: "dom",
+        e: "end",
+        f: "forEach"
+    };
 
+    return {
+        state
+    }
+})();
 
-// create finished list
-function createPartListHTMLTemp(arr) {
-    let htmlTmp = "";
-    arr.forEach((ele) => {
-      if (ele.isTask !== true) {
-        htmlTmp += `<div class="row">
-              <div class="left">
-                <input type="checkbox" id="list1" name="list1" />
-                <label for="list1"> ${ele.title}</label>
-              </div>
-              <div class="right">
-                <button class="overdue-indicator"><span> ${ele.counter} Overdue</span></button>
-                <button class="duesoon-indicator"><span> ${ele.counter} Due Soon</span></button>
-              </div>
-              </div>`;
-      }
-    });
-    return htmlTmp;
-  }
+const Controler = ((view, model) => {
+    const createKeyList = () => {
+        let tmp = '';
+        Object.keys(model.state).forEach(key => {
+            tmp += `<option value="${key}">${key}</option>`;
+        });
+        view.render(keys, tmp);
+    }
+    const createValueList = () => {
+        let tmp = '';
+        Object.keys(model.state).forEach(key => {
+            tmp += `<option value="${key}">${model.state[key]}</option>`;
+        });
+        view.render(values, tmp);
+    }
+    const setUpEvent = () => {
+        view.domElements.keys.addEventListener('change', event => {
+            view.domElements.values.value = event.target.value;
+        });
+        view.domElements.values.addEventListener('change', event => {
+            view.domElements.keys.value = event.target.value;
+        });
+    }
 
-// toggle button????
-function changeButton(arr) {
-    const buttonDueSoon = document.querySelectorAll(".duesoon-indicator");
-    const buttonOverdue = document.querySelectorAll(".overdue-indicator");
-    buttonDueSoon.forEach((ele, i) => {
-      if (arr[i].isOverdue === true) {
-        //   console.log("yes");
-        buttonOverdue[i].classList.add("display-overdue");
-        buttonDueSoon[i].classList.add("hidden");
-      } else {
-        buttonOverdue[i].classList.add("hidden");
-        buttonDueSoon[i].classList.add("display-duesoon");
-      }
-    });
-  }
+    const init = () => {
+        createKeyList();
+        createValueList();
+        setUpEvent();
+    }
+    return { init };
+})(View, Model);
 
-// render counter
-// make toggle work
-let htmlListTmp;
-const bottomEle = document.querySelector(".bottom");
-
-const checkbox = document.getElementById("myonoffswitch");
-htmlListTmp = createPartListHTMLTemp(itemArr);
-bottomEle.innerHTML = htmlListTmp;
-
-function render(checkbox) {
-  if (checkbox.checked === true) {
-    htmlListTmp = createAllListHTMLTemp(itemArr);
-    bottomEle.innerHTML = htmlListTmp;
-    changeButton(itemArr);
-  } else {
-    htmlListTmp = createPartListHTMLTemp(itemArr);
-    bottomEle.innerHTML = htmlListTmp;
-    changeButton(itemArr);
-  }
-}
-
-changeButton(itemArr);
+Controler.init();
